9c0b345498b8ec64ff2cb7216fa01fa549c7bb6d
gabriel7mendes <andrewsseven@gmail.com>
2021-07-14 17:48:57 -0300

    15. Conditional Statements Using "when"

--- src/conditionals/Conditionals.kt
+++ src/conditionals/Conditionals.kt
@@ -1,6 +1,7 @@
 package conditionals
 
 fun main(args: Array<String>) {
+   /*
     val age: Int = 24
     if(age < 18) {
         println("You cannot register.")
@@ -13,4 +14,17 @@ fun main(args: Array<String>) {
     }
 
     println("The end.")
+    */
+
+    val mode: Int = 5
+
+    when(mode) {
+        1 -> println("The mode is lazy.")
+        2 -> {
+            println("The mode is 2.")
+            println("So the mode is busy.")
+        }
+        3 -> println("The mode is super-productive.")
+        else -> println("I don't know that mode.")
+    }
 }
