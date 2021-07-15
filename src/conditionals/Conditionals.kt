5a4df9b0d48db1fb1be23da7977e86d1e711cb2c
gabriel7mendes <andrewsseven@gmail.com>
2021-07-15 09:57:47 -0300

    17. Conditional Expressions 18. More Advanced "when" Constructs

--- src/conditionals/Conditionals.kt
+++ src/conditionals/Conditionals.kt
@@ -16,6 +16,7 @@ fun main(args: Array<String>) {
     println("The end.")
     */
 
+    /*
     val mode: Int = 5
 
     when(mode) {
@@ -26,5 +27,26 @@ fun main(args: Array<String>) {
         }
         3 -> println("The mode is super-productive.")
         else -> println("I don't know that mode.")
+    }
+
+    val x = if(mode < 2) {
+        println("Mode is less than 2")
+        17
+    } else {
+        42
+    }
+
+    println(x)
+    */
+
+    val x = 9
+    when(x) {
+        5 -> println("x is 5")
+        3*12 -> println("x is 3*12")
+        "Hey there".length -> println("x is the length of the string 'Hey there'")
+        in 1..10 -> println("x is between 1 and 10")
+        in 11..20 -> println()
+        in 21..30 -> println()
+        !in 1..9 -> println("x is not between 1 to 9")
     }
 }
