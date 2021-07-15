4e18d3a9dbcbd48c79d0ed0bab28b02952c561d0
gabriel7mendes <andrewsseven@gmail.com>
2021-07-15 10:19:56 -0300

    21. Arrays in Kotlin

--- src/collections/ArraysLists.kt
+++ src/collections/ArraysLists.kt
@@ -2,6 +2,26 @@ package collections
 
 fun main(args: Array<String>) {
     val array = arrayOf("Texas", "Iowa", "California")
+    val mixed = arrayOf("Kotlin", 117, 3.0, false)
+    val numbers = intArrayOf(1,2,3,4,5)
+    println(mixed[0])
+    mixed[2] = 3.1415
+    println(mixed[2])
+    val str = "Udemy"
+    println(str[0])
+
+    val states = arrayOf("Nevada", "Florida")
+    val allStates = array + states
+
+    println(allStates.size)
+
+    val empty: Boolean = numbers.isEmpty()
+
+    if(states.contains("Kentucky")) {
+        println("It contains Kentucky")
+    } else {
+        println("It does not")
+    }
 
     val list = listOf("Orange", "Apple", "Bananas")
 
