ed7754770ad11644b7c05489b052bc1e90ca9e01
gabriel7mendes <andrewsseven@gmail.com>
2021-07-15 10:06:33 -0300

    20. Arrays vs. Lists

---
+++ src/collections/ArraysLists.kt
@@ -0,0 +1,10 @@
+package collections
+
+fun main(args: Array<String>) {
+    val array = arrayOf("Texas", "Iowa", "California")
+
+    val list = listOf("Orange", "Apple", "Bananas")
+
+    val arrayList = arrayListOf("Patrick", "Michael", "Sarah")
+    arrayList.add("Sandra")
+}
