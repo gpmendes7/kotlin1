5f6c42e6318c747a86b8595b6f22bd8be00e5db5
gabriel7mendes <andrewsseven@gmail.com>
2021-07-19 15:05:43 -0300

    34. Methods

--- src/oo/FirstClass.kt
+++ src/oo/FirstClass.kt
@@ -3,10 +3,29 @@ package oo
 class Person {
     var name: String = "Sarah"
     var age: Int = 42
+
+    fun speak() {
+        println("Hello!")
+    }
+
+    fun greet(name: String) {
+        println("Hello $name!")
+    }
+
+//    fun getYearOfBirth(): Int {
+//        return 2016 = age
+//    }
+
+    fun getYearOfBirth() = 2016 - age
 }
 
 fun main(args: Array<String>) {
     val person = Person()
+
+    person.speak();
+    person.greet("world")
+    println(person.getYearOfBirth())
+
     println(person.name)
     println(person.age)
 
